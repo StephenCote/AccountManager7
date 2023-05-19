b0a49c7a26efb35ffed17676cd2ecea653b90981
Stephen Cote <sw.cote@gmail.com>
2023-05-19 16:43:23 -0500

    Add search, patch, delete, and get end points

--- src/AccountManagerService7/src/main/java/org/cote/rest/services/ModelService.java
+++ src/AccountManagerService7/src/main/java/org/cote/rest/services/ModelService.java
@@ -7,7 +7,11 @@ import javax.annotation.security.DeclareRoles;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -21,6 +25,8 @@ import org.cote.accountmanager.exceptions.ModelNotFoundException;
 import org.cote.accountmanager.exceptions.ReaderException;
 import org.cote.accountmanager.exceptions.ValueException;
 import org.cote.accountmanager.io.IOSystem;
+import org.cote.accountmanager.io.Query;
+import org.cote.accountmanager.io.QueryResult;
 import org.cote.accountmanager.io.ParameterList;
 import org.cote.accountmanager.model.field.FieldType;
 import org.cote.accountmanager.objects.generated.PolicyResponseType;
@@ -42,6 +48,54 @@ import org.cote.service.util.ServiceUtil;
 public class ModelService {
 	private static final Logger logger = LogManager.getLogger(ModelService.class);
 
+	@GET
+	@Path("/{type:[A-Za-z]+}/{objectId:[0-9A-Za-z\\\\-]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getModelByObjectId(@PathParam("type") String type, @PathParam("objectId") String objectId, @Context HttpServletRequest request, @Context HttpServletResponse response){
+		BaseRecord user = ServiceUtil.getPrincipalUser(request);
+		BaseRecord rec = IOSystem.getActiveContext().getAccessPoint().findByObjectId(user, type, objectId);
+
+		if(rec == null) {
+			return Response.status(404).entity(null).build();
+		}
+
+		return Response.status(200).entity(rec.toFilteredString()).build();
+	}
+	
+	@DELETE
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteModel(String json, @Context HttpServletRequest request, @Context HttpServletResponse response){
+		BaseRecord user = ServiceUtil.getPrincipalUser(request);
+		BaseRecord imp = JSONUtil.importObject(json,  LooseRecord.class, RecordDeserializerConfig.getFilteredModule());
+
+		if(imp == null) {
+			return Response.status(404).entity(null).build();
+		}
+
+		boolean deleted = IOSystem.getActiveContext().getAccessPoint().delete(user, imp);
+		return Response.status(200).entity(deleted).build();
+	}
+	
+	@PATCH
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response patchModel(String json, @Context HttpServletRequest request, @Context HttpServletResponse response){
+		
+		BaseRecord user = ServiceUtil.getPrincipalUser(request);
+		BaseRecord imp = JSONUtil.importObject(json,  LooseRecord.class, RecordDeserializerConfig.getFilteredModule());
+
+		boolean patched = false;
+		if(imp == null) {
+			return Response.status(404).entity(null).build();
+		}
+
+		if(IOSystem.getActiveContext().getAccessPoint().update(user, imp) != null) {
+			patched = true;
+		}
+		return Response.status(200).entity(patched).build();
+	}
+	
 	@POST
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -56,61 +110,50 @@ public class ModelService {
 		BaseRecord op = null;
 		BaseRecord oop = null;
 		List<String> outFields = new ArrayList<>();
-		//PolicyType pol = null;
+
 		try {
-			
-
-			ParameterList plst = ParameterUtil.newParameterList();
-			if(imp.hasField(FieldNames.FIELD_NAME)) {
-				ParameterUtil.newParameter(plst, FieldNames.FIELD_NAME, imp.get(FieldNames.FIELD_NAME));
-			}
-			if(imp.hasField(FieldNames.FIELD_GROUP_PATH)) {
-				ParameterUtil.newParameter(plst, FieldNames.FIELD_PATH, imp.get(FieldNames.FIELD_GROUP_PATH));
-			}
-			if(imp.hasField(FieldNames.FIELD_PATH)) {
-				ParameterUtil.newParameter(plst, FieldNames.FIELD_PATH, imp.get(FieldNames.FIELD_PATH));
-			}
-
-
-			op = IOSystem.getActiveContext().getFactory().newInstance(imp.getModel(), user, null, plst);
-
-			for(FieldType f : imp.getFields()) {
-				FieldSchema sf = schema.getFieldSchema(f.getName());
-				if(!sf.isIdentity() && !sf.isVirtual()) {
-					logger.info("Mapping " + f.getName());
-					op.set(f.getName(),  imp.get(f.getName()));
-				}
-			}
-			// logger.info(JSONUtil.exportObject(user, RecordSerializerConfig.getUnfilteredModule()));
-			// logger.info(JSONUtil.exportObject(op, RecordSerializerConfig.getUnfilteredModule()));
-			if(IOSystem.getActiveContext().getPolicyUtil().createPermitted(user, user, op)) {
-				if(IOSystem.getActiveContext().getRecordUtil().createRecord(op)) {
-					
-					for(FieldSchema f : schema.getFields()) {
-						if(f.isIdentity()) {
-							outFields.add(f.getName());
-						}
-					}
-					
-				}
-				else {
-					logger.error("Failed to create record");
-					op = null;
-				}
-			}
-			else {
-				logger.error("Create is not permitted");
+			op = IOSystem.getActiveContext().getFactory().newInstance(imp.getModel(), user, imp, null);
+			oop = IOSystem.getActiveContext().getAccessPoint().create(user, op);
+			if(oop == null) {
+				logger.error("Failed to create record");
 				op = null;
 			}
-
-			
-		} catch (FactoryException | FieldException | ValueException | ModelNotFoundException e) {
+			else {
+				for(FieldType f : oop.getFields()) {
+					FieldSchema fs = schema.getFieldSchema(f.getName());
+					if(fs.isIdentity()) {
+						outFields.add(f.getName());
+					}
+				}
+			}
+		}
+		catch (FactoryException  e) {
 			logger.error(e);
 		}
 
 		String ops = null;
-		if(op != null) {
-			ops = JSONUtil.exportObject(op.copyRecord(outFields.toArray(new String[0])), RecordSerializerConfig.getFilteredModule());
+		if(oop != null) {
+			ops = oop.copyRecord(outFields.toArray(new String[0])).toFilteredString();
+		}
+		return Response.status(200).entity(ops).build();
+	}
+	
+	@POST
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response search(String json, @Context HttpServletRequest request, @Context HttpServletResponse response){
+		
+		BaseRecord user = ServiceUtil.getPrincipalUser(request);
+		BaseRecord imp = JSONUtil.importObject(json,  LooseRecord.class, RecordDeserializerConfig.getFilteredModule());
+		if(imp == null) {
+			return Response.status(404).entity(null).build();
+		}
+		Query query = new Query(imp);
+		
+		QueryResult qr = IOSystem.getActiveContext().getAccessPoint().list(user, query);
+		String ops = null;
+		if(qr != null) {
+			ops = qr.toFilteredString();
 		}
 		return Response.status(200).entity(ops).build();
 	}
