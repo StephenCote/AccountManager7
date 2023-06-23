6044ee1d54ac052625f008c32c05235e3874a19b
Stephen Cote <sw.cote@gmail.com>
2023-06-23 08:57:57 -0500

    Refactor the membership test to use the new API

--- src/AccountManagerObjects7/src/test/java/org/cote/accountmanager/objects/tests/TestMembership.java
+++ src/AccountManagerObjects7/src/test/java/org/cote/accountmanager/objects/tests/TestMembership.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +15,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.cote.accountmanager.exceptions.DatabaseException;
 import org.cote.accountmanager.exceptions.FieldException;
 import org.cote.accountmanager.exceptions.IndexException;
 import org.cote.accountmanager.exceptions.ModelException;
@@ -18,7 +23,14 @@ import org.cote.accountmanager.exceptions.ModelNotFoundException;
 import org.cote.accountmanager.exceptions.ReaderException;
 import org.cote.accountmanager.exceptions.ValueException;
 import org.cote.accountmanager.factory.Factory;
+import org.cote.accountmanager.io.IOSystem;
+import org.cote.accountmanager.io.OrganizationContext;
+import org.cote.accountmanager.io.Query;
+import org.cote.accountmanager.io.QueryResult;
 import org.cote.accountmanager.io.QueryUtil;
+import org.cote.accountmanager.io.db.DBReader;
+import org.cote.accountmanager.io.db.DBStatementMeta;
+import org.cote.accountmanager.io.db.StatementUtil;
 import org.cote.accountmanager.objects.generated.FactType;
 import org.cote.accountmanager.objects.generated.PolicyDefinitionType;
 import org.cote.accountmanager.objects.generated.PolicyRequestType;
@@ -26,141 +38,135 @@ import org.cote.accountmanager.objects.generated.PolicyResponseType;
 import org.cote.accountmanager.objects.generated.PolicyType;
 import org.cote.accountmanager.policy.PolicyDefinitionUtil;
 import org.cote.accountmanager.policy.PolicyEvaluator;
+import org.cote.accountmanager.policy.PolicyUtil;
 import org.cote.accountmanager.record.BaseRecord;
 import org.cote.accountmanager.record.LooseRecord;
 import org.cote.accountmanager.record.RecordDeserializerConfig;
+import org.cote.accountmanager.record.RecordFactory;
 import org.cote.accountmanager.record.RecordSerializerConfig;
 import org.cote.accountmanager.schema.AccessSchema;
 import org.cote.accountmanager.schema.FieldNames;
 import org.cote.accountmanager.schema.ModelNames;
 import org.cote.accountmanager.schema.type.PolicyResponseEnumType;
+import org.cote.accountmanager.schema.type.RoleEnumType;
 import org.cote.accountmanager.util.JSONUtil;
 import org.cote.accountmanager.util.ResourceUtil;
 import org.junit.Test;
 
 public class TestMembership extends BaseTest {
-	/*
-	@Test
-	public void TestBlank() {
-		logger.info("Blank test");
-		logger.info("Use -DtrimStackTrace=false to find stackoverflow in mvn test");
-	}
-	*/
-	/*
-	@Test
-	public void TestMemberIndex() {
-
-		BaseRecord testMember1 = null;
-		BaseRecord homeDir = null;
-		BaseRecord testDat1 = null;
-		Factory mf = ioContext.getFactory();
-		try 
-		{
-			testMember1 = mf.getCreateUser(orgContext.getAdminUser(), "testMember1", orgContext.getOrganizationId());
-			homeDir = ioContext.getReader().read(ModelNames.MODEL_GROUP, (long)testMember1.get(FieldNames.FIELD_HOME_DIRECTORY_FIELD_ID));
-					
-					//ioContext.getSearch().find(QueryUtil.createIdentityQuery(ModelNames.MODEL_GROUP, FieldNames.FIELD_ID, testMember1.get(FieldNames.FIELD_HOME_DIRECTORY_FIELD_ID)));
-			
-			testDat1 = getCreateData(testMember1, "Test Data - " + UUID.randomUUID().toString(), "text/plain", "This is the demo data".getBytes(), "~/Data/Membership Test", (long)orgContext.getOrganizationId());
-		}
-		catch(Exception e) {
-			logger.error(e);
-			
-		}
-		// logger.info("Has name: " + homeDir.hasField(FieldNames.FIELD_NAME));
-		// logger.info(JSONUtil.exportObject(homeDir, RecordSerializerConfig.getUnfilteredModule()));
-		// logger.info(JSONUtil.exportObject(testDat1, RecordSerializerConfig.getUnfilteredModule()));
-	}
-	*/
 	
 	@Test
-	public void TestMemberIndex() {
-		logger.error("**** REFACTOR FOR REVISED MEMBER UTIL");
-		/*
-		BaseRecord testMember1 = null;
-		BaseRecord testMember2 = null;
-		BaseRecord testMember3 = null;
+	public void TestMembershipList() {
+		OrganizationContext testOrgContext = getTestOrganization("/Development/Test Membership");
+		
 		Factory mf = ioContext.getFactory();
-		try 
-		{
-			
-			testMember1 = mf.getCreateUser(orgContext.getAdminUser(), "testMember1", orgContext.getOrganizationId());
-			testMember2 = mf.getCreateUser(orgContext.getAdminUser(), "testMember2", orgContext.getOrganizationId());
-			testMember3 = mf.getCreateUser(orgContext.getAdminUser(), "testMember3", orgContext.getOrganizationId());
-		}
-		catch(Exception e) {
-			logger.error(e);
-			
-		}
+		BaseRecord testUser1 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser1", testOrgContext.getOrganizationId());
+		BaseRecord testUser2 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser2", testOrgContext.getOrganizationId());
+		BaseRecord testUser3 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser3", testOrgContext.getOrganizationId());
+		setupAccountReader(testUser1);
+		setupAccountReader(testUser3);
+		BaseRecord crole1 = ioContext.getPathUtil().makePath(testUser1, ModelNames.MODEL_ROLE, "~/MemberRoles/Lister", "USER", testOrgContext.getOrganizationId());
+		BaseRecord crole3 = ioContext.getPathUtil().makePath(testUser3, ModelNames.MODEL_ROLE, "~/MemberRoles/Lister", "USER", testOrgContext.getOrganizationId());
 		
-		
-		BaseRecord dat = getCreateData(testMember1, "Test Membership Data 1", "text/plain", "This is the demo data".getBytes(), "~/Data/Membership Test", (long)orgContext.getOrganizationId());
-		BaseRecord urole1 = mf.userRole(testMember1);
-		BaseRecord uperm1 = mf.userPermission(testMember1);
-		assertNotNull("User role is null", urole1);
-		assertNotNull("User permission is null", uperm1);
-		BaseRecord cperm1 = ioContext.getPathUtil().makePath(testMember1, ModelNames.MODEL_PERMISSION, "~/MemberRights/Read", "USER", orgContext.getOrganizationId());
-		BaseRecord crole1 = ioContext.getPathUtil().makePath(testMember1, ModelNames.MODEL_ROLE, "~/MemberRoles/Reader", "USER", orgContext.getOrganizationId());
-		BaseRecord crole2p = ioContext.getPathUtil().makePath(testMember1, ModelNames.MODEL_ROLE, "~/MemberRoles/Parent", "USER", orgContext.getOrganizationId());
-		BaseRecord crole2c = ioContext.getPathUtil().makePath(testMember1, ModelNames.MODEL_ROLE, "~/MemberRoles/Parent/Child", "USER", orgContext.getOrganizationId());
-		
-		assertNotNull("Custom permission is null", cperm1);
-		// logger.info(JSONUtil.exportObject(cperm1, RecordSerializerConfig.getUnfilteredModule()));
-		
-		logger.info("Add testMember2 to Reader role");
-		ioContext.getMemberUtil().member(testMember1, crole1, testMember2, null, false);
-		boolean addMember = ioContext.getMemberUtil().member(testMember1, crole1, testMember2, null, true);
-		assertTrue("Expected member to be added", addMember);
+		BaseRecord rec = IOSystem.getActiveContext().getPathUtil().findPath(testUser1, ModelNames.MODEL_ROLE, "~/", RoleEnumType.USER.toString(), testOrgContext.getOrganizationId());
+		assertNotNull("Role is null", rec);
+		logger.info(rec.toFullString());
+		boolean canRead = ioContext.getPolicyUtil().readPermitted(testUser1, testUser1, null, rec);
+		logger.info("Can read: " + canRead);
+		Query qp = QueryUtil.buildQuery(testUser1, ModelNames.MODEL_ROLE, rec.get(FieldNames.FIELD_OBJECT_ID), null, 0L, 0);
+		assertNotNull("Query is null", qp);
 
-		ioContext.getMemberUtil().member(testMember1, crole2p, testMember3, null, false);
-		ioContext.getMemberUtil().member(testMember1, crole2p, testMember3, null, true);
-		
-		logger.info("Give Reader role Read permission to Test Data");
-		ioContext.getMemberUtil().member(testMember1, dat, crole1, cperm1, false);
-		boolean addRole = ioContext.getMemberUtil().member(testMember1, dat, crole1, cperm1, true);
-		assertTrue("Expected role to be added", addRole);
-		
-		ioContext.getMemberUtil().member(testMember1, dat, crole2c, cperm1, false);
-		ioContext.getMemberUtil().member(testMember1, dat, crole2c, cperm1, true);
-		
-		BaseRecord u2parts = null;
-		BaseRecord r1parts = null;
-		List<BaseRecord> u2parts2 = new ArrayList<>();
-		try {
-			u2parts = ioContext.getMemberUtil().getParticipants(testMember2);
-			r1parts = ioContext.getMemberUtil().getMembers(crole1);
-			u2parts2 = ioContext.getMemberUtil().findMembers(crole1, ModelNames.MODEL_USER, testMember2.get(FieldNames.FIELD_ID));
-		} catch (IndexException | ReaderException e) {
-			logger.error(e);
-			
-		}
-		assertNotNull("Expected a parts list", u2parts);
-		assertNotNull("Expected a parts list", r1parts);
-		assertTrue("Expected one find result", u2parts2.size() == 1);
-		
-		List<BaseRecord> parts = u2parts.get(FieldNames.FIELD_PARTS);
-		
-		assertTrue("Expected 1 participant and received " + parts.size(), parts.size() == 1);
-		BaseRecord part1 = parts.get(0);
-		assertTrue("Expect part id to be the role id", part1.get(FieldNames.FIELD_PART_ID).equals(crole1.get(FieldNames.FIELD_ID)));
+		ioContext.getAccessPoint().member(testUser1, crole1, testUser2, null, false);
+		boolean mem1 = ioContext.getAccessPoint().member(testUser1, crole1, testUser2, null, true);
+		assertTrue("Expected to set membership", mem1);
 
-		List<BaseRecord> parts2 = r1parts.get(FieldNames.FIELD_PARTS);
-		assertTrue("Expected 1 participant", parts2.size() == 1);
-		BaseRecord part2 = parts2.get(0);
-		assertTrue("Expect part id to be the role id", part2.get(FieldNames.FIELD_PART_ID).equals(testMember2.get(FieldNames.FIELD_ID)));
-		//logger.info(JSONUtil.exportObject(part2, RecordSerializerConfig.getUnfilteredModule()));
+		/// Cycling through membership by first returning to false - when checking the return value of the member function, it only returns true when the value changed, not an indication of status
+		///
+		ioContext.getAccessPoint().member(testUser3, crole3, testUser1, null, false);
+		ioContext.getAccessPoint().member(testUser3, crole3, testUser1, null, true);
+		ioContext.getAccessPoint().member(testUser3, crole3, testUser2, null, false);
+		ioContext.getAccessPoint().member(testUser3, crole3, testUser2, null, true);
 		
-		boolean mem1 = ioContext.getMemberUtil().isMember(testMember2, crole1);
-		boolean mem2 = ioContext.getMemberUtil().isMember(testMember3, crole1);
-		boolean mem3 = ioContext.getMemberUtil().isMember(testMember3, crole2c, true);
-		assertTrue("Expected user to be a member", mem1);
-		assertFalse("Didn't expect user to be a member", mem2);
-		assertTrue("Expected user to be a member", mem3);
+		Query q = QueryUtil.createQuery(ModelNames.MODEL_USER);
+		q.filterParticipation(crole1, ModelNames.MODEL_USER, null);
+		logger.info(q.key());
 		
-		boolean check = ioContext.getAuthorizationUtil().checkEntitlement(testMember2, cperm1, dat);
-		logger.info("Entitlement check: " + check);
-		*/
+		QueryResult qr = ioContext.getAccessPoint().list(testUser1, q);
+		assertNotNull("Result was null", qr);
+		logger.info("Test direct list of query - result count: " + qr.getResults().length);
+		assertTrue("Expected one member", qr.getResults().length == 1);
+		
+		logger.info("Test listMembers interface, which constructs the query");
+		int count = ioContext.getAccessPoint().countMembers(testUser1, crole1, ModelNames.MODEL_USER, null);
+		logger.info("Count: " + count);
+		assertTrue("Expected count to be 1", count == 1);
+		List<BaseRecord> recs = ioContext.getAccessPoint().listMembers(testUser1, crole1, ModelNames.MODEL_USER, null, 0, 10);
+		assertTrue("Expected one member", recs.size() == 1);
 	}
+	
+	private void setupAccountReader(BaseRecord user) {
+		OrganizationContext ctx = ioContext.getOrganizationContext(user.get(FieldNames.FIELD_ORGANIZATION_PATH), null);
+		BaseRecord urole = AccessSchema.getSystemRole(AccessSchema.ROLE_ACCOUNT_USERS_READERS, RoleEnumType.USER.toString(), ctx.getOrganizationId());
+		assertNotNull("User Reader role is null", urole);
+		ioContext.getAccessPoint().member(
+				ctx.getAdminUser(),
+				urole,
+				user, null, true
+		);
+		assertTrue("Expected user to be a member of the role", IOSystem.getActiveContext().getAccessPoint().isMember(ctx.getAdminUser(), urole, user));
+	}
+
+	@Test
+	public void TestMemberIndex() {
+		OrganizationContext testOrgContext = getTestOrganization("/Development/Test Membership");
+		Factory mf = ioContext.getFactory();
+		BaseRecord testUser1 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser1", testOrgContext.getOrganizationId());
+		BaseRecord testUser2 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser2", testOrgContext.getOrganizationId());
+		BaseRecord testUser3 =  mf.getCreateUser(testOrgContext.getAdminUser(), "testUser3", testOrgContext.getOrganizationId());
+
+		setupAccountReader(testUser1);
+
+		/*
+		BaseRecord urole = AccessSchema.getSystemRole(AccessSchema.ROLE_ACCOUNT_USERS_READERS, RoleEnumType.USER.toString(), testOrgContext.getOrganizationId());
+		assertNotNull("User Reader role is null", urole);
+		ioContext.getAccessPoint().member(
+				testOrgContext.getAdminUser(),
+				urole,
+				testUser1, null, true
+		);
+		assertTrue("Expected user to be a member of the role", IOSystem.getActiveContext().getAccessPoint().isMember(testOrgContext.getAdminUser(), urole, testUser1));
+		*/
+		
+		BaseRecord crole1 = ioContext.getPathUtil().makePath(testUser1, ModelNames.MODEL_ROLE, "~/MemberRoles/Reader", "USER", testOrgContext.getOrganizationId());
+		BaseRecord crole2p = ioContext.getPathUtil().makePath(testUser1, ModelNames.MODEL_ROLE, "~/MemberRoles/Parent", "USER", testOrgContext.getOrganizationId());
+		BaseRecord crole2c = ioContext.getPathUtil().makePath(testUser1, ModelNames.MODEL_ROLE, "~/MemberRoles/Parent/Child", "USER", testOrgContext.getOrganizationId());
+		assertNotNull("Role is null", crole2c);
+		
+		PolicyResponseType prr = IOSystem.getActiveContext().getAuthorizationUtil().canRead(testUser1, testUser1, testUser2);
+		assertTrue("Expected user1 to be able to read user2", prr.getType() == PolicyResponseEnumType.PERMIT);
+
+		PolicyResponseType prr2 = IOSystem.getActiveContext().getAuthorizationUtil().canRead(testUser2, testUser2, testUser1);
+		assertTrue("Expected user2 to not be able to read user1", prr2.getType() == PolicyResponseEnumType.DENY);
+
+		
+		ioContext.getAccessPoint().member(testUser1, crole2c, testUser2, null, false);
+		ioContext.getAccessPoint().member(testUser1, crole2c, testUser3, null, false);
+		boolean mem1 = ioContext.getAccessPoint().member(testUser1, crole2c, testUser2, null, true);
+		assertTrue("Expected to set membership", mem1);
+		mem1 = ioContext.getAccessPoint().member(testUser1, crole2c, testUser3, null, true);
+		assertTrue("Expected to set membership", mem1);
+		
+		List<BaseRecord> mems = new ArrayList<>();
+		try {
+			mems = ioContext.getMemberUtil().findMembers(crole2c, ModelNames.MODEL_USER, 0L);
+		} catch (IndexException | ReaderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue("Expected 2 members", mems.size() == 2);
+	
+	}
+
 
 	
 	
