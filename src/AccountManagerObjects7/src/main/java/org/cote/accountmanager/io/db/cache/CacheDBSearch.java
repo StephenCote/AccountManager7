f3fde4e3aa206549fa4e833c5b9aa86b5a7c6085
Stephen Cote <sw.cote@gmail.com>
2023-06-30 09:06:24 -0500

    Debugging concurrent modification and cache corruption
    There's a really nasty bug where concurrently modifying the query cache is causing the incorrect value to be returned, even though it enters the cache correctly.  Possibly due to entries being removed from the cache, but still being investigated.

--- src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/io/db/cache/CacheDBSearch.java
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/io/db/cache/CacheDBSearch.java
@@ -3,6 +3,7 @@ package org.cote.accountmanager.io.db.cache;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.cote.accountmanager.cache.CacheUtil;
 import org.cote.accountmanager.cache.ICache;
@@ -18,22 +19,22 @@ import org.cote.accountmanager.schema.FieldNames;
 import org.cote.accountmanager.util.RecordUtil;
 
 public class CacheDBSearch extends DBSearch implements ICache {
+	
+	/// 2023/06/29 - There is a currency problem with this rather simplistic cache when put under load
+	/// For some reason, the mapped values become transposed somehow
 
-	private Map<String, QueryResult> cache = Collections.synchronizedMap(new HashMap<>());
+	private Map<String, QueryResult> cache = new ConcurrentHashMap<>();
+	// Collections.synchronizedMap(new HashMap<>());
 	public CacheDBSearch(DBReader reader) {
 		super(reader);
 		CacheUtil.addProvider(this);
 	}
 
 	public void clearCache() {
-		synchronized(cache) {
-			cache.clear();
-		}
+		cache.clear();
 	}
 	public void clearCache(String key) {
-		synchronized(cache) {
-			cache.remove(key);
-		}
+		cache.remove(key);
 	}
 	public void cleanupCache() {
 		clearCache();
@@ -47,50 +48,68 @@ public class CacheDBSearch extends DBSearch implements ICache {
 	}
 	
 	@Override
-	public QueryResult find(Query query) throws IndexException, ReaderException {
-		QueryResult res = null;
-		String hash = query.hash();
-		if(cache.containsKey(hash)) {
-			// logger.info("Cache hit: " + query.key());
-			return cache.get(hash);
-		}
-		
-		// logger.info(query.key());
-		
-		res = super.find(query);
-		if(res != null && res.getCount() > 0) {
-			synchronized(cache) {
-				cache.put(hash, res);
+	public QueryResult find(final Query query) throws IndexException, ReaderException {
+
+			if(query.key() == null) {
+				throw new ReaderException("Null query key");
 			}
-		}
-		return res;
+			String hash = query.hash();
+			if(cache.containsKey(hash)) {
+				// logger.info("Cache hit: " + query.hash() + " " + query.key());
+				final QueryResult qr = cache.get(hash);
+				String qt = query.get(FieldNames.FIELD_TYPE);
+				String qrt = qr.get(FieldNames.FIELD_TYPE);
+				if(!qt.equals(qrt)) {
+					logger.error("****** MISMATCHED CACHED RESULT TYPE!!! " + qt + " -> " + qrt);
+					logger.error("Query hash: " + hash + " / " + query.hash() + " / " + qr.get(FieldNames.FIELD_QUERY_HASH));
+					logger.error(query.toFullString());
+					logger.error(qr.toFullString());
+					throw new ReaderException("Mismatched result type: Cached query for " + qt + " contains " + qrt);
+				}
+				return qr;
+				//return cache.get(hash);
+			}
+			
+			// logger.info(query.key());
+
+				final QueryResult res = super.find(query);
+				if(res != null && res.getCount() > 0) {
+					String qt = query.get(FieldNames.FIELD_TYPE);
+					String qrt = res.get(FieldNames.FIELD_TYPE);
+					if(!qt.equals(qrt)) {
+						logger.error("****** MISMATCHED RESULT TYPE ENTERING CACHE!!! " + qt + " -> " + qrt);
+						logger.error("Query hash: " + hash + " / " + query.hash() + " / " + res.get(FieldNames.FIELD_QUERY_HASH));
+						logger.error(query.toFullString());
+						logger.error(res.toFullString());
+		
+					}
+					cache.put(hash, res);
+				}
+				return res;
+
 	}
 
 	@Override
-	public void clearCache(BaseRecord rec) {
+	public synchronized void clearCache(BaseRecord rec) {
 		// logger.info("TODO: Clear cache by record");
-		synchronized(cache) {
-			cache.entrySet().removeIf(entry ->{
-				QueryResult mr = entry.getValue();
-				boolean match = false;
-				for(BaseRecord r : mr.getResults()) {
-					if(rec.getModel().equals(r.getModel()) && RecordUtil.matchIdentityRecords(rec, r)) {
-						// logger.info("Clear record based on query result");
-						match = true;
-						break;
-					}
-					
+		cache.entrySet().removeIf(entry ->{
+			QueryResult mr = entry.getValue();
+			boolean match = false;
+			for(BaseRecord r : mr.getResults()) {
+				if(rec.getModel().equals(r.getModel()) && RecordUtil.matchIdentityRecords(rec, r)) {
+					// logger.info("Clear record based on query result");
+					match = true;
+					break;
 				}
-				return match;
-			});
-		}
+				
+			}
+			return match;
+		});
 	}
 	
 	@Override
 	public void clearCacheByModel(String model) {
-		synchronized(cache) {
-			cache.values().removeIf(entry -> model.equals((String)entry.get(FieldNames.FIELD_TYPE)));
-		}
+		cache.values().removeIf(entry -> model.equals((String)entry.get(FieldNames.FIELD_TYPE)));
 	}
 
 	@Override
