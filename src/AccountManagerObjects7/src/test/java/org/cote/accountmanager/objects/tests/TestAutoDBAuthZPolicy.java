79822255f53d186d5ebf776546eae44b265b714b
Stephen Cote <sw.cote@gmail.com>
2023-09-01 08:43:28 -0500

    Initial test for auto-generating authorization procedure from policy

---
+++ src/AccountManagerObjects7/src/test/java/org/cote/accountmanager/objects/tests/TestAutoDBAuthZPolicy.java
@@ -0,0 +1,15 @@
+package org.cote.accountmanager.objects.tests;
+
+import org.cote.accountmanager.policy.PolicyUtil;
+import org.cote.accountmanager.util.ResourceUtil;
+import org.junit.Test;
+
+public class TestAutoDBAuthZPolicy extends BaseTest{
+
+	@Test
+	public void TestAuthZGen() {
+		String policyBase = ioContext.getPolicyUtil().getPolicyBase(PolicyUtil.POLICY_SYSTEM_READ_OBJECT);
+		logger.info(policyBase);
+	}
+	
+}
