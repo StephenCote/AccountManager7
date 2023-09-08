2d80742533e228ba3745044639c6fb7f5a2db210
Stephen Cote <sw.cote@gmail.com>
2023-09-08 08:52:26 -0500

    Add a maintenance thread to manage active websockets and notifications

---
+++ src/AccountManagerService7/src/main/java/org/cote/service/threads/NotificationThread.java
@@ -0,0 +1,40 @@
+package org.cote.service.threads;
+
+import java.util.List;
+
+import javax.websocket.Session;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cote.accountmanager.record.BaseRecord;
+import org.cote.accountmanager.thread.Threaded;
+import org.cote.sockets.WebSocketService;
+
+
+public class NotificationThread extends Threaded {
+	
+	public static final Logger logger = LogManager.getLogger(NotificationThread.class);
+	private int spoolFlushDelay = 300000;
+	private int counter = 0;
+	public NotificationThread(){
+		super();
+		this.setThreadDelay(spoolFlushDelay);
+	}
+	public void execute(){
+    	counter++;
+    	//List<BaseRecord> users = WebSocketService.activeUsers();
+    	List<Session> sessions = WebSocketService.activeSessions();
+		//logger.info("Chirping " + sessions.size() + " sessions: " + counter);
+    	sessions.forEach(session ->{
+    		BaseRecord user = WebSocketService.getUserBySession(session);
+    		String[] msg = new String[] {"Heartbeat - " + counter};
+    		if(user != null) {
+    			WebSocketService.chirpUser(user, msg);
+    		}
+    		else {
+    			WebSocketService.sendMessage(session, msg, false, false, false);
+    		}
+    	});
+	}
+	
+}
