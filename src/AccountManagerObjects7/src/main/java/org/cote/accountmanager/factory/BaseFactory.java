530898c5b2282f89f5cf9aa95ceff90b1ed349e7
Stephen Cote <sw.cote@gmail.com>
2023-11-10 08:26:26 -0600

    Code cleanup

--- src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/factory/BaseFactory.java
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/factory/BaseFactory.java
@@ -22,12 +22,6 @@ public class BaseFactory extends FactoryBase {
 		if(contextUser != null) {
 			try {
 				IOSystem.getActiveContext().getRecordUtil().applyOwnership(contextUser, newRecord, contextUser.get(FieldNames.FIELD_ORGANIZATION_ID));
-				//(new MemoryReader()).read(newRecord);
-				/*
-				long oid = contextUser.get(FieldNames.FIELD_ID);
-				newRecord.set(FieldNames.FIELD_OWNER_ID, oid);
-				newRecord.set(FieldNames.FIELD_ORGANIZATION_ID, contextUser.get(FieldNames.FIELD_ORGANIZATION_ID));
-				*/
 			} catch (FieldException | ValueException | ModelNotFoundException e) {
 				throw new FactoryException(e);
 			}
