5d3618358a6fc575753b08cbd298088cdb64c934
Stephen Cote <sw.cote@gmail.com>
2023-11-23 09:30:37 -0600

    Placeholder for default rules
    This will ultimately be its own model attached to a world instance.

---
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/olio/Rules.java
@@ -0,0 +1,8 @@
+package org.cote.accountmanager.olio;
+
+public class Rules {
+
+	public static final int INITIAL_STATISTICS_ALLOTMENT = 110;
+	public static final int INITIAL_MINIMUM_STATISTIC = 1;
+	public static final int MAXIMUM_STATISTIC = 20;
+}
