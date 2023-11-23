997a955f1ecdf3cb876d98a709c04354fe6d5901
Stephen Cote <sw.cote@gmail.com>
2023-11-23 09:32:39 -0600

    Stub utility for handling character statistics

---
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/olio/StatisticsUtil.java
@@ -0,0 +1,87 @@
+package org.cote.accountmanager.olio;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cote.accountmanager.exceptions.FieldException;
+import org.cote.accountmanager.exceptions.ModelNotFoundException;
+import org.cote.accountmanager.exceptions.ReaderException;
+import org.cote.accountmanager.exceptions.ValueException;
+import org.cote.accountmanager.io.IOSystem;
+import org.cote.accountmanager.io.MemoryReader;
+import org.cote.accountmanager.record.BaseRecord;
+import org.cote.accountmanager.schema.ModelNames;
+
+public class StatisticsUtil {
+	public static final Logger logger = LogManager.getLogger(StatisticsUtil.class);
+	private static SecureRandom rand = new SecureRandom();
+	private static final StatisticRule[] statistics = new StatisticRule[]{
+		new StatisticRule("physicalStrength"), new StatisticRule("physicalEndurance"), new StatisticRule("manualDexterity"), new StatisticRule("agility"), new StatisticRule("speed"), new StatisticRule("mentalStrength"),
+		new StatisticRule("mentalEndurance"), new StatisticRule("intelligence"), new StatisticRule("wisdom"), new StatisticRule("charisma"), new StatisticRule("fae", 4)
+	};
+	
+	public static void rollStatistics(BaseRecord rec) {
+		rollStatistics(rec, 0);
+	}
+	public static void rollStatistics(BaseRecord rec, int age) {
+		if(!rec.inherits(ModelNames.MODEL_CHAR_STATISTICS)) {
+			logger.error("Record is not a statistics record");
+			return;
+		}
+		IOSystem.getActiveContext().getReader().populate(rec);
+		int maxStat = Rules.MAXIMUM_STATISTIC;
+		if(age > 0 && age < 13) {
+			maxStat = 10;
+		}
+		
+		/// Every statistic receives a minimum of 1
+		///  - (statistics.length * Rules.INITIAL_MINIMUM_STATISTIC)
+		int allotment = Rules.INITIAL_STATISTICS_ALLOTMENT;
+		int total = 0;
+		try {
+			List<StatisticRule> slist = Arrays.asList(statistics);
+			Collections.shuffle(slist);
+			for(StatisticRule stat : slist) {
+				int max = Math.max(Math.min(allotment, maxStat), 1);
+				int rstat = Math.max(rand.nextInt(max), 1) + Rules.INITIAL_MINIMUM_STATISTIC;
+				allotment -= rstat;
+				total += rstat;
+				rec.set(stat.getName(), (rstat * stat.getMultiplier()));
+			}
+			// logger.info("Allotted: " + total + " / Remainder: " + allotment);
+			if(allotment > 0) {
+				rec.set("potential", allotment);
+			}
+			
+			/// Invoke inspect to perform any calculations on virtual fields
+			///
+			(new MemoryReader()).inspect(rec);
+		}
+		catch(ModelNotFoundException | FieldException | ValueException | ReaderException e) {
+			logger.error(e);
+		}
+		
+	}
+	
+}
+class StatisticRule{
+	private String name = null;
+	private int multiplier = 0;
+	public StatisticRule(String name){
+		this(name, 1);
+	}
+	public StatisticRule(String name, int mult){
+		this.name = name;
+		this.multiplier = mult;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getMultiplier() {
+		return multiplier;
+	}
+}
