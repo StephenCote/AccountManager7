7096b85f3756a8c30f5076f40e92d74bd5f222d1
Stephen Cote <sw.cote@gmail.com>
2023-12-01 15:13:00 -0600

    Separate getAttribute from getAttributeValue

--- src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/util/AttributeUtil.java
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/util/AttributeUtil.java
@@ -3,6 +3,8 @@ package org.cote.accountmanager.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.cote.accountmanager.exceptions.FieldException;
 import org.cote.accountmanager.exceptions.ModelException;
 import org.cote.accountmanager.exceptions.ModelNotFoundException;
@@ -13,7 +15,8 @@ import org.cote.accountmanager.schema.FieldNames;
 import org.cote.accountmanager.schema.ModelNames;
 
 public class AttributeUtil {
-
+	public static final Logger logger = LogManager.getLogger(AttributeUtil.class);
+	
 	public static <T> BaseRecord addAttribute(BaseRecord record, String name, T val) throws ModelException, FieldException, ModelNotFoundException, ValueException {
 		if(!record.inherits(ModelNames.MODEL_ATTRIBUTE_LIST)) {
 			throw new ModelException("Model does not inherit from attribute");
@@ -33,10 +36,25 @@ public class AttributeUtil {
 		if(record.hasField(FieldNames.FIELD_ID)) {
 			attr.set(FieldNames.FIELD_REFERENCE_ID, record.get(FieldNames.FIELD_ID));
 		}
+		if(record.hasField(FieldNames.FIELD_ORGANIZATION_ID)) {
+			attr.set(FieldNames.FIELD_ORGANIZATION_ID, record.get(FieldNames.FIELD_ORGANIZATION_ID));
+		}
 		attr.set(FieldNames.FIELD_REFERENCE_TYPE, record.getModel());
 		return attr;
 	}
-	
+	public static BaseRecord getAttribute(BaseRecord record, String name) {
+		BaseRecord attr = null;
+		List<BaseRecord> attrs = record.get(FieldNames.FIELD_ATTRIBUTES);
+		List<BaseRecord> mattrs = attrs.stream().filter(o ->{
+			String attrName = o.get(FieldNames.FIELD_NAME);
+			return name.equals(attrName);
+		}).collect(Collectors.toList());
+		if(mattrs.size() > 0) {
+			attr = mattrs.get(0);
+		}
+
+		return attr;
+	}
 	public static <T> T getAttributeValue(BaseRecord record, String name) throws ModelException {
 		return getAttributeValue(record, name, null);
 	}
@@ -44,16 +62,21 @@ public class AttributeUtil {
 		if(!record.inherits(ModelNames.MODEL_ATTRIBUTE_LIST)) {
 			throw new ModelException("Model does not inherit from attribute");
 		}
-		List<BaseRecord> attrs = record.get(FieldNames.FIELD_ATTRIBUTES);
-		List<BaseRecord> mattrs = attrs.stream().filter(o ->{
-			String attrName = o.get(FieldNames.FIELD_NAME);
-			return name.equals(attrName);
-		}).collect(Collectors.toList());
 		T outT = defVal;
-		
-		if(mattrs.size() > 0) {
-			outT = mattrs.get(0).get(FieldNames.FIELD_VALUE);
+
+		try {
+			BaseRecord attr = getAttribute(record, name);
+			
+			if(attr != null) {
+				outT = attr.get(FieldNames.FIELD_VALUE);
+			}
 		}
+		catch(Exception e) {
+			logger.error(e);
+			logger.error(record.getModel() + ".attribute." + name);
+			e.printStackTrace();
+		}
+
 		return outT;
 	}
 	
