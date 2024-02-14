6e4eaeace7c4da5a4d8130fc17cfa1cb520f139a
Stephen Cote <sw.cote@gmail.com>
2024-02-13 19:27:31 -0600

    Add dark triad description to narrative description

--- src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/olio/NarrativeUtil.java
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/olio/NarrativeUtil.java
@@ -19,6 +19,30 @@ import org.cote.accountmanager.schema.type.TerrainEnumType;
 public class NarrativeUtil {
 	public static final Logger logger = LogManager.getLogger(NarrativeUtil.class);
 	
+	public static String getDarkTriadDescription(PersonalityProfile prof) {
+		StringBuilder desc = new StringBuilder();
+		StringBuilder desc2 = new StringBuilder();
+		if(prof.isMachiavellian()) {
+			desc.append("is machiavellian and may be callous, lack morality, and/or are motivated by self-interest");
+		}
+		
+		if(prof.isNarcissist()) {
+			if(desc.length() > 0) desc.append("; ");
+			desc.append("is narcissistic and may show grandiosity, entitlement, dominance, and/or superiority");
+		}
+		if(prof.isPsychopath()) {
+			if(desc.length() > 0) desc.append("; ");
+			desc.append("is a psychopath and may show low levels of empathy and high levels of impulsivity and thrill-seeking");
+		}
+		desc2.append("Personality-wise, " + prof.getRecord().get("firstName") + " ");
+		if(desc.length() > 0) {
+			desc2.append(desc.toString() + ".");
+		}
+		else {
+			desc2.append("does not reflect dark traits.");
+		}
+		return desc2.toString();
+	}
 	public static String getRaceDescription(List<String> races) {
 		StringBuilder desc = new StringBuilder();
 		for(String rc: races) {
@@ -62,6 +86,7 @@ public class NarrativeUtil {
 		buff.append(fname + " is a " + age + " year old " + raceDesc + " " + ("male".equals(gender) ? "man" : "woman") + ".");
 		buff.append(" " + pro + " has " + eyeColor + " eyes and " + hairColor + " " + hairStyle + " hair.");
 		buff.append(" " + pro + " is a '" + pp.getMbtiTitle() + "' and is " + pp.getMbtiDescription() + ".");
+		buff.append(" " + getDarkTriadDescription(pp));
 		buff.append(" " + pro + " is");
 		if(stets.size() > 0) {
 			buff.append(" standing on a patch of " + tet.toString().toLowerCase() + " near " + stets.stream().collect(Collectors.joining(",")) + ".");
@@ -115,9 +140,10 @@ public class NarrativeUtil {
 			if(id == (long)p.get("id")) {
 				continue;
 			}
+			PersonalityProfile pp2 = ProfileUtil.analyzePersonality(ctx, p);
 			String compatKey = OCEANUtil.getCompatibilityKey(pov.get("personality"), p.get("personality"));
 			CompatibilityEnumType mbtiCompat = MBTIUtil.getCompatibility(pov.get("personality.mbtiKey"), p.get("personality.mbtiKey"));
-			buff.append("\n" + p.get("firstName") + " " + getRaceDescription(p.get("race")) + " (" + p.get("gender") + "): " + compatKey + " / " + mbtiCompat.toString());
+			buff.append("\n" + p.get("firstName") + " " + getRaceDescription(p.get("race")) + " (" + p.get("age") + " year old " + p.get("gender") + "): " + compatKey + " / " + mbtiCompat.toString() + " / " + getDarkTriadDescription(pp2));
 		}
 		
 		return buff.toString();
