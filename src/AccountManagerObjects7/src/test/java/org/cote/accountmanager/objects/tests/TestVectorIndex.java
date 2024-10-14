86f82f8dc62240eee633b92b9fe3d19ddd400a75
Stephen Cote <sw.cote@gmail.com>
2024-10-14 07:53:00 -0500

    BROKEN: Testing PGVector and DJL
    Pytorch dependencies currently incorrect resulting in a failed test.

---
+++ src/AccountManagerObjects7/src/test/java/org/cote/accountmanager/objects/tests/TestVectorIndex.java
@@ -0,0 +1,358 @@
+package org.cote.accountmanager.objects.tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.cote.accountmanager.io.IOFactory;
+import org.cote.accountmanager.io.IOSystem;
+import org.cote.accountmanager.objects.tests.olio.OlioTranslator;
+import org.cote.accountmanager.record.RecordFactory;
+import org.cote.accountmanager.schema.ModelNames;
+import org.cote.accountmanager.schema.ModelSchema;
+import org.junit.Test;
+import com.pgvector.PGvector;
+
+import ai.djl.Device;
+import ai.djl.MalformedModelException;
+import ai.djl.ModelException;
+import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.nlp.DefaultVocabulary;
+import ai.djl.modality.nlp.Vocabulary;
+import ai.djl.modality.nlp.bert.BertToken;
+import ai.djl.modality.nlp.bert.BertTokenizer;
+import ai.djl.modality.nlp.qa.QAInput;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelNotFoundException;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.DeferredTranslatorFactory;
+import ai.djl.translate.TranslateException;
+public class TestVectorIndex extends BaseTest {
+
+	 public static final String HYBRID_SQL = """
+			    WITH semantic_search AS (
+			        SELECT id, RANK () OVER (ORDER BY embedding <=> ?) AS rank
+			        FROM vecdoc
+			        ORDER BY embedding <=> ?
+			        LIMIT 20
+			    ),
+			    keyword_search AS (
+			        SELECT id, RANK () OVER (ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC)
+			        FROM vecdoc, plainto_tsquery('english', ?) query
+			        WHERE to_tsvector('english', content) @@ query
+			        ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC
+			        LIMIT 20
+			    )
+			    SELECT
+			        COALESCE(semantic_search.id, keyword_search.id) AS id,
+			        COALESCE(1.0 / (? + semantic_search.rank), 0.0) +
+			        COALESCE(1.0 / (? + keyword_search.rank), 0.0) AS score
+			    FROM semantic_search
+			    FULL OUTER JOIN keyword_search ON semantic_search.id = keyword_search.id
+			    ORDER BY score DESC
+			    LIMIT 5
+			    """;
+	
+	 // https://pub.towardsai.net/deploy-huggingface-nlp-models-in-java-with-deep-java-library-e36c635b2053
+	 // https://gist.github.com/KexinFeng/97e6344556f88822650d023acfbdf4f5
+	
+	private String predict(OlioTranslator ot, QAInput input) throws ModelNotFoundException, MalformedModelException, IOException, TranslateException {
+		Criteria<QAInput, String> criteria = Criteria.builder()
+				  .setTypes(QAInput.class, String.class)
+				  .optModelPath(Paths.get(testProperties.getProperty("test.datagen.path") + "/nlp/trace_cased_bertqa.pt"))
+				  .optTranslator(ot)
+				  .build();
+				ZooModel<QAInput, String> model = criteria.loadModel();
+				Predictor<QAInput, String> predictor = model.newPredictor(ot);
+				return predictor.predict(input);
+	}
+	
+	private String getPDF(String path) {
+		String output = null;
+		  PDDocument doc;
+		try {
+			doc = Loader.loadPDF(new File(path));
+			output = new PDFTextStripper().getText(doc);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  
+		return output;
+	}
+	
+	private List<String> chunkText(String block, int chunkSize) {
+		/// .map(s -> s.trim())
+		List<String> sents = Arrays.asList(block.split("((?<=\\.)|(?=\\.))")).stream().filter(s -> s.length() > 0).collect(Collectors.toList());
+		final AtomicInteger counter = new AtomicInteger(0);
+		return sents.stream().collect(Collectors.groupingBy(s -> counter.getAndIncrement()/chunkSize)).values().stream().flatMap(List::stream).collect(Collectors.toList());
+	}
+	
+	private ZooModel<String, float[]> zooModel = null;
+	private ZooModel<String, float[]> getZooModel(){
+		if(zooModel == null) {
+			try {
+				zooModel = loadModel(testProperties.getProperty("test.datagen.path") + "/nlp/torchscript/bert-base-cased-squad2");
+			} catch (IOException | ModelException e) {
+				logger.error(e);
+				e.printStackTrace();
+			}
+		}
+		return zooModel;
+	}
+	
+	
+	private void loadDocument(String documentId, String documentData) {
+		List<String> chunks = chunkText(documentData, 20).stream().collect(Collectors.toList());
+		loadDocument(documentId, chunks);
+	}
+	private void loadDocument(String documentId, List<String> chunks) {
+		if(documentExists(documentId)) {
+			logger.warn("Document exists");
+			return;
+		}
+		try (Connection con = ioContext.getDbUtil().getDataSource().getConnection()){
+			PGvector.addVectorType(con);
+			logger.info("Generating " + chunks.size() + " embeddings");
+            List<float[]> embeddings = generateEmbeddings(getZooModel(), chunks.toArray(new String[0]));
+            int size = chunks.size();
+            for (int i = 0; i < chunks.size(); i++) {
+                PreparedStatement insertStmt = con.prepareStatement("INSERT INTO vecdoc (documentId, chunk, chunkCount, content, embedding) VALUES (?, ?, ?, ?, ?)");
+                insertStmt.setString(1, documentId);
+                insertStmt.setInt(2, (i + 1));
+                insertStmt.setInt(3, size);
+                insertStmt.setString(4, chunks.get(i));
+                insertStmt.setObject(5, new PGvector(embeddings.get(i)));
+                insertStmt.executeUpdate();
+            }
+            
+		}
+		catch(SQLException | TranslateException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+	}
+
+	private String getDocument(String id, int start, int length, boolean bidi) {
+		int upd = 0;
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT content FROM vecdoc WHERE documentId = ?");
+		if(length > 0) {
+			sql.append(" AND chunk => ? ANd chunk < ?");
+		}
+		List<String> doc = new ArrayList<>();
+		try (Connection con = ioContext.getDbUtil().getDataSource().getConnection()){
+	            PreparedStatement queryStmt = con.prepareStatement(sql.toString());
+	            queryStmt.setString(1, id);
+	            if(length > 0) {
+	            	queryStmt.setInt(2, start - (bidi ? length : 0));
+	            	queryStmt.setInt(3, start + length);
+	            }
+	            ResultSet rs = queryStmt.executeQuery();
+	            while(rs.next()) {
+	            	doc.add(rs.getString(1));
+	            }
+	            rs.close();
+	            
+		}
+		catch(SQLException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+		return doc.stream().collect(Collectors.joining());
+	}
+	
+	private int deleteDocument(String id) {
+		int upd = 0;
+		try (Connection con = ioContext.getDbUtil().getDataSource().getConnection()){
+	            PreparedStatement queryStmt = con.prepareStatement("DELETE FROM vecdoc WHERE documentId = ?");
+	            queryStmt.setString(1, id);
+	            upd = queryStmt.executeUpdate();
+		}
+		catch(SQLException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+		return upd;
+	}
+	
+	private boolean documentExists(String id) {
+		boolean exists = false;
+		try (Connection con = ioContext.getDbUtil().getDataSource().getConnection()){
+	            PreparedStatement queryStmt = con.prepareStatement("SELECT count(id) FROM vecdoc WHERE documentId = ?");
+	            queryStmt.setString(1, id);
+	            ResultSet rs = queryStmt.executeQuery();
+	            if(rs.next()) {
+	            	exists = (rs.getInt(1) > 0);
+	            }
+	            rs.close();
+		}
+		catch(SQLException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+		return exists;
+	}
+	
+	@Test
+	public void TestPDF() {
+		logger.info("Testing PDF");
+		String pdfText = getPDF("./media/CardFox.pdf");
+		assertNotNull("Text was null", pdfText);
+		logger.info("Text len: " + pdfText.length());
+
+		//logger.info("Begin translating ...");
+		if(documentExists("CardFox")) {
+			deleteDocument("CardFox");
+		}
+		loadDocument("CardFox", pdfText);
+		
+		/*
+		List<String> chunks = chunkText(pdfText, 15);
+		logger.info("Chunks: " + chunks.size());
+		//List<String> sents = Arrays.asList(pdfText.split("\\.")).stream().map(s -> s.trim()).filter(s -> s.length() > 0).collect(Collectors.toList());
+		assertTrue("Expected chunks", chunks.size() > 0);
+		String chunk1 = chunks.get(0);
+		logger.info("Chunk 1 " + chunk1.length() + " " + chunk1);
+		//logger.info("Sents: " + sents.length);
+		
+		String output = translate("What are the character names?", chunk1);
+		logger.info("End translating ...");
+		logger.info("Translation: " + output);
+		*/
+		/*
+		try {
+			ZooModel<String, float[]> model = loadModel(testProperties.getProperty("test.datagen.path") + "/nlp/torchscript/bert-base-cased-squad2");
+
+            List<float[]> embeddings = generateEmbeddings(model, new String[] {pdfText});
+            logger.info("Emb Size: " + embeddings.size());
+            logger.info("Emb Dimen: " + embeddings.get(0).length);
+		}
+		catch(ModelException | IOException | TranslateException e) {
+			e.printStackTrace();
+		}
+		*/
+	}
+	
+	private String translate(String question, String content) {
+		OlioTranslator ot = new OlioTranslator(testProperties.getProperty("test.datagen.path") + "/nlp/bert-base-cased-vocab.txt");
+		
+		QAInput input = new QAInput(question, content);
+		String output = null;
+		try {
+			output = predict(ot, input);
+		} catch (ModelNotFoundException | MalformedModelException | IOException | TranslateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return output;
+	}
+	 
+	@Test
+	public void TestTokenizer() {
+		logger.info("Testing tokenizer");
+		String question = "When did BBC Japan start broadcasting?";
+		String resourceDocument = 
+		  "BBC Japan was a general entertainment Channel.\n" + 
+		  "Which operated between December 2004 and April 2006.\n" + 
+		  "It ceased operations after its Japanese distributor folded.";
+		
+		String output = translate(question, resourceDocument);
+		logger.info("Output: " + output);
+	}
+	 
+	@Test
+	public void TestVectorStore() {
+		logger.info("Testing vector store");
+		
+		// ModelSchema ms = RecordFactory.getSchema(ModelNames.MODEL_DATA);
+		// logger.info(IOSystem.getActiveContext().getDbUtil().generateSchema(ms));
+		
+		try (Connection con = ioContext.getDbUtil().getDataSource().getConnection()){
+	        PGvector.addVectorType(con);
+	        ZooModel<String, float[]> model = loadModel(testProperties.getProperty("test.datagen.path") + "/nlp/torchscript/bert-base-cased-squad2");
+	        String[] input = {
+	                "The dog is barking",
+	                "The cat is purring",
+	                "The bear is growling"
+	            };
+	            List<float[]> embeddings = generateEmbeddings(model, input);
+
+	            for (int i = 0; i < input.length; i++) {
+	                PreparedStatement insertStmt = con.prepareStatement("INSERT INTO vecdoc (content, embedding) VALUES (?, ?)");
+	                insertStmt.setString(1, input[i]);
+	                insertStmt.setObject(2, new PGvector(embeddings.get(i)));
+	                insertStmt.executeUpdate();
+	            }
+	            
+	            String query = "growling bear";
+	            float[] queryEmbedding = generateEmbeddings(model, new String[] {query}).get(0);
+	            double k = 60;
+
+	            PreparedStatement queryStmt = con.prepareStatement(HYBRID_SQL);
+	            queryStmt.setObject(1, new PGvector(queryEmbedding));
+	            queryStmt.setObject(2, new PGvector(queryEmbedding));
+	            queryStmt.setString(3, query);
+	            queryStmt.setDouble(4, k);
+	            queryStmt.setDouble(5, k);
+	            ResultSet rs = queryStmt.executeQuery();
+	            while (rs.next()) {
+	                System.out.println(String.format("document: %d, RRF score: %f", rs.getLong("id"), rs.getDouble("score")));
+	            }
+		}
+		catch(SQLException | IOException | ModelException | TranslateException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+
+	}
+	
+	/// From https://github.com/pgvector/pgvector-java/blob/master/examples/djl/src/main/java/com/example/Example.java
+	/// https://djl.ai/extensions/tokenizers/ for bert-base-cased-squad2
+    private static ZooModel<String, float[]> loadModel(String id) throws IOException, ModelException {
+    	//logger.info(id + " / " + Paths.get(id));
+        return Criteria.builder()
+            .setTypes(String.class, float[].class)
+            //.optModelUrls("djl://ai.djl.huggingface.pytorch/" + id)
+            // 384
+           //.optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/all-MiniLM-L6-v2")
+            // 1024
+            .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/bert-large-nli-cls-token")
+            // 768 - but produces very large dimensions 
+            //.optModelUrls("djl://ai.djl.huggingface.pytorch/bert-base-uncased")
+            .optEngine("PyTorch")
+            //.optDevice(Device.gpu()) 
+            .optTranslatorFactory(new TextEmbeddingTranslatorFactory())
+            .build()
+            .loadModel();
+    }
+    
+    private static List<float[]> generateEmbeddings(ZooModel<String, float[]> model, String[] input) throws TranslateException {
+        Predictor<String, float[]> predictor = model.newPredictor();
+        List<float[]> embeddings = new ArrayList<>(input.length);
+        logger.info(embeddings.size());
+        for (String text : input) {
+        	float[] f = predictor.predict(text);
+            embeddings.add(f);
+        }
+        return embeddings;
+    }
+}
---
+++ src/AccountManagerObjects7/src/test/java/org/cote/accountmanager/objects/tests/olio/OlioTranslator.java
@@ -0,0 +1,87 @@
+package org.cote.accountmanager.objects.tests.olio;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import ai.djl.modality.nlp.DefaultVocabulary;
+import ai.djl.modality.nlp.Vocabulary;
+import ai.djl.modality.nlp.bert.BertToken;
+import ai.djl.modality.nlp.bert.BertTokenizer;
+import ai.djl.modality.nlp.qa.QAInput;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
+import ai.djl.translate.Batchifier;
+import ai.djl.translate.Translator;
+import ai.djl.translate.TranslatorContext;
+
+
+// https://pub.towardsai.net/deploy-huggingface-nlp-models-in-java-with-deep-java-library-e36c635b2053
+public class OlioTranslator implements Translator<QAInput, String> {
+	  private List<String> tokens;
+	  private Vocabulary vocabulary;
+	  private BertTokenizer tokenizer;
+	  
+	  private String vocabFile = null;
+	  public OlioTranslator(String vocabFile) {
+		  this.vocabFile = vocabFile;
+	  }
+	  
+	  @Override
+	  public void prepare(TranslatorContext ctx) {
+	    Path path = Paths.get(vocabFile);
+	    try {
+		    vocabulary = DefaultVocabulary.builder()
+		                .optMinFrequency(1)
+		                .addFromTextFile(path)
+		                .optUnknownToken("[UNK]")
+		                .build();
+	    }
+	    catch(IOException e) {
+	    	e.printStackTrace();
+	    }
+	    tokenizer = new BertTokenizer();
+	  }
+	    
+	  @Override
+	  public NDList processInput(TranslatorContext ctx, QAInput input){
+	    BertToken token =
+	      tokenizer.encode(
+	        input.getQuestion().toLowerCase(),
+	        input.getParagraph().toLowerCase()
+	      );
+	    // get the encoded tokens used in precessOutput
+	    tokens = token.getTokens();
+	    NDManager manager = ctx.getNDManager();
+	    // map the tokens(String) to indices(long)
+	    long[] indices =
+	      tokens.stream().mapToLong(vocabulary::getIndex).toArray();
+	    long[] attentionMask = 
+	      token.getAttentionMask().stream().mapToLong(i -> i).toArray();
+	    long[] tokenType = token.getTokenTypes().stream()
+	      .mapToLong(i -> i).toArray();
+	    NDArray indicesArray = manager.create(indices);
+	    NDArray attentionMaskArray =
+	      manager.create(attentionMask);
+	    NDArray tokenTypeArray = manager.create(tokenType);
+	    // The order matters
+	    return new NDList(indicesArray, attentionMaskArray,
+	      tokenTypeArray);
+	  }
+	    
+	  @Override
+	  public String processOutput(TranslatorContext ctx, NDList list) {
+	    NDArray startLogits = list.get(0);
+	    NDArray endLogits = list.get(1);
+	    int startIdx = (int) startLogits.argMax().getLong();
+	    int endIdx = (int) endLogits.argMax().getLong();
+	    return tokenizer.tokenToString(tokens.subList(startIdx, endIdx + 1));
+	  }
+	    
+	  @Override
+	  public Batchifier getBatchifier() {
+	    return Batchifier.STACK;
+	  }
+	}
