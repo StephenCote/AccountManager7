762481b062d940124bbee1989b680df916f8037b
Stephen Cote <sw.cote@gmail.com>
2025-01-19 12:11:18 -0600

    Cleanup

--- src/AccountManagerUx7/client/components/navigation.js
+++ src/AccountManagerUx7/client/components/navigation.js
@@ -1,19 +1,19 @@
-(function(){
+(function () {
     const navigation = {};
     let space;
     let vnode;
     let entityName = "sig";
     let app;
 
-    function navigable(){
+    function navigable() {
         let nav = {
-            drawerOpen : false,
-            contextMenus : [],
-            pendingMenus : [],
-            cleanupMenus : [],
-            init : function(){
+            drawerOpen: false,
+            contextMenus: [],
+            pendingMenus: [],
+            cleanupMenus: [],
+            init: function () {
                 document.addEventListener("keydown", e => {
-                    if (e.keyCode == 27){
+                    if (e.keyCode == 27) {
                         page.endDialog();
                         this.drawer(1);
                         this.menu(null, true);
@@ -21,207 +21,191 @@
                 });
 
             },
-            addDrawerClickEvent : function(eventPath){
-                document.querySelector(eventPath).onclick = function(){
+            addDrawerClickEvent: function (eventPath) {
+                document.querySelector(eventPath).onclick = function () {
                     nav.drawer();
                 };
             },
-            removeContextMenu : function(menuPath){
+            removeContextMenu: function (menuPath) {
                 let len = this.contextMenus.length;
-                this.contextMenus = this.contextMenus.filter(function(o) { return o.menuPath != menuPath; });
-                // console.log("Cleanup " + menuPath + " = " + (this.contextMenus.length < len));
+                this.contextMenus = this.contextMenus.filter(function (o) { return o.menuPath != menuPath; });
             },
-            /*
-            contextMenuButton2: function(sLabel, sIco, fHandler){
-                return m("button",{class : "context-menu-item", onclick : function(){page.navigable.menu(".context-menu");if(fHandler) fHandler();}},[(sLabel || ""), (sIco ? m("span",{class : "material-icons material-icons-cm ml-2"},sIco) : "")]);
-            },
-            */
-            contextMenuButton: function(sMenuId, sLabel, sIco, fHandler){
+            contextMenuButton: function (sMenuId, sLabel, sIco, fHandler) {
                 let id = sMenuId;
-                if(!sMenuId.match(/^\./)) id = "#" + sMenuId;
-                //console.log(id);
-                return m("button",{class : "context-menu-item", onclick : 
-                    function(){
-                        
-                        page.navigable.menu(id);
-                        if(fHandler) fHandler();
-                }
-                },[(sLabel || ""), (sIco ? m("span",{class : "material-symbols-outlined material-symbols-outlined-cm ml-2"},sIco) : "")]);
+                if (!sMenuId.match(/^\./)) id = "#" + sMenuId;
+                return m("button", {
+                    class: "context-menu-item", onclick:
+                        function () {
+
+                            page.navigable.menu(id);
+                            if (fHandler) fHandler();
+                        }
+                }, [(sLabel || ""), (sIco ? m("span", { class: "material-symbols-outlined material-symbols-outlined-cm ml-2" }, sIco) : "")]);
             },
-            cleanupContextMenus : function(){
-                navg.cleanupMenus.forEach((m)=>{
+            cleanupContextMenus: function () {
+                navg.cleanupMenus.forEach((m) => {
                     navg.removeContextMenu(m);
                 });
                 navg.cleanupMenus = [];
             },
-            setupPendingContextMenus : function(){
-                // console.log("Create menus ... " + navg.pendingMenus.length);
-                navg.pendingMenus.forEach((o)=>{
+            setupPendingContextMenus: function () {
+                navg.pendingMenus.forEach((o) => {
                     navg.addContextMenu(o.menu, o.button, o.query, o.class);
                     navg.cleanupMenus.push(o.menu);
                 });
                 navg.pendingMenus = [];
             },
-            pendContextMenu : function(menuPath, buttonPath, vQuery, altClass){
-                navg.pendingMenus.push({menu: menuPath, button: buttonPath, class: altClass, query: vQuery});
+            pendContextMenu: function (menuPath, buttonPath, vQuery, altClass) {
+                navg.pendingMenus.push({ menu: menuPath, button: buttonPath, class: altClass, query: vQuery });
             },
-            addContextMenu : function(menuPath, buttonPath, vQuery, sAltCls){
+            addContextMenu: function (menuPath, buttonPath, vQuery, sAltCls) {
                 let oE = document.querySelector(buttonPath);
-                if(oE == null){
+                if (oE == null) {
                     console.warn(buttonPath + " does not exist");
                     return;
                 }
                 let ctx = {
-                    menuOpen : false,
-                    buttonPath : buttonPath,
-                    menuPath : menuPath,
+                    menuOpen: false,
+                    buttonPath: buttonPath,
+                    menuPath: menuPath,
                     class: sAltCls,
-                    query : vQuery
+                    query: vQuery
                 };
                 this.contextMenus.push(ctx);
 
-                oE.onclick = function(){
-                    (!vQuery ? Promise.resolve() : nav.populateContextMenu(vQuery)).then(()=>{
+                oE.onclick = function () {
+                    (!vQuery ? Promise.resolve() : nav.populateContextMenu(vQuery)).then(() => {
                         nav.menu(menuPath);
                     });
                 };
             },
-            menu : function(path, bF, bN){
-                this.contextMenus.forEach((m)=>{
-                    //console.log(m.menuPath + " == " + path + " / " + bN + " / " + (!path || m.menuPath === path) + " / " + (path && !bF && !m.menuOpen) );
-                    //if(!path || m.menuPath === path) m.menuOpen = (path && !bF && !m.menuOpen) || false;
+            menu: function (path, bF, bN) {
+                this.contextMenus.forEach((m) => {
                     m.menuOpen = (
                         (!path || m.menuPath === path)
                         && !bF && !m.menuOpen) || false;
                 });
-                if(!bN) this.flash();
+                if (!bN) this.flash();
             },
-            drawer : function(bF){
+            drawer: function (bF) {
                 this.drawerOpen = (!bF && !this.drawerOpen) || false;
-                if(this.drawerOpen) this.menu(0, true);
+                if (this.drawerOpen) this.menu(0, true);
                 this.flash();
             },
-            glass: function(){
+            glass: function () {
                 let cls = "screen-glass screen-glass-slide screen-glass-open";
                 document.querySelector("div.screen-glass").className = cls;
             },
-            flash : function(){
+            flash: function () {
                 let cls = "screen-glass screen-glass-slide screen-glass-" + (this.drawerOpen ? 'open' : 'close');
                 document.querySelector("div.screen-glass").className = cls;
                 document.querySelector("aside.transition").className = "transition transition-" + (this.drawerOpen ? 'full' : '0');
-                this.contextMenus.forEach((m)=>{
+                this.contextMenus.forEach((m) => {
                     let cls = m.class || "context-menu-48";
                     let mnu = document.querySelector(m.menuPath);
-                    if(mnu == null){
+                    if (mnu == null) {
                         console.debug("Null menu for path: " + m.menuPath);
                     }
                     else mnu.className = cls + " transition transition-" + (m.menuOpen ? 'full' : '0');
                 });
             },
-            populateContextMenu : function(vQuery){
-                
+            populateContextMenu: function (vQuery) {
+
                 /// There isn't an attribute to control visibility (vs. read permission), and the long-standing default has been to preface groups to be hidden with a period
                 ///
                 let ignoreList = /^\./;
 
-                return (vQuery.loaded ? Promise.resolve() : new Promise((res,rej)=>{
+                return (vQuery.loaded ? Promise.resolve() : new Promise((res, rej) => {
                     vQuery.loaded = true;
-                    (vQuery.objectId ? Promise.resolve(vQuery.objectId) : new Promise((res2,rej2)=>{
-                        am7client.find(vQuery.type,vQuery.subType,vQuery.path,function(v){
-                            if(!v){
+                    (vQuery.objectId ? Promise.resolve(vQuery.objectId) : new Promise((res2, rej2) => {
+                        am7client.find(vQuery.type, vQuery.subType, vQuery.path, function (v) {
+                            if (!v) {
                                 vQuery.denied = true;
                                 res2();
                             }
                             else res2(v.objectId);
                         })
                     }))
-                .then((gid)=>{
-                        //console.log("Listing in " + gid);
-                        vQuery.objectId = gid;
-                        if(vQuery.items) res();
-                        else{
-                            am7client.list(vQuery.type, gid, null, 0, 0, function(r){
-                                let oMenu = document.querySelector("#" + vQuery.menu);
-                                oMenu.innerHTML = "";
-                                //console.log("Got - " + r.length);
-                                vQuery.items = r;
-                                r.forEach((g)=>{
-                                    if(!g.name.match(ignoreList)){
-                                        let btn = document.createElement("button");
-                                        btn.setAttribute("class","context-menu-item");
-                                        btn.appendChild(document.createTextNode(g.name));
-                                        if(vQuery.icon){
-                                            let ico = document.createElement("span");
-                                            ico.setAttribute("class","material-symbols-outlined material-icons-cm ml-2");
-                                            ico.appendChild(document.createTextNode(vQuery.icon));
-                                            btn.appendChild(ico);
+                        .then((gid) => {
+                            vQuery.objectId = gid;
+                            if (vQuery.items) res();
+                            else {
+                                am7client.list(vQuery.type, gid, null, 0, 0, function (r) {
+                                    let oMenu = document.querySelector("#" + vQuery.menu);
+                                    oMenu.innerHTML = "";
+                                    vQuery.items = r;
+                                    r.forEach((g) => {
+                                        if (!g.name.match(ignoreList)) {
+                                            let btn = document.createElement("button");
+                                            btn.setAttribute("class", "context-menu-item");
+                                            btn.appendChild(document.createTextNode(g.name));
+                                            if (vQuery.icon) {
+                                                let ico = document.createElement("span");
+                                                ico.setAttribute("class", "material-symbols-outlined material-icons-cm ml-2");
+                                                ico.appendChild(document.createTextNode(vQuery.icon));
+                                                btn.appendChild(ico);
+                                            }
+                                            oMenu.appendChild(btn);
+                                            btn.onclick = function () {
+                                                nav.menu("#" + vQuery.menu);
+                                                if (vQuery.handler) vQuery.handler(vQuery, g);
+                                            };
                                         }
-                                        oMenu.appendChild(btn);
-                                        btn.onclick = function(){
-                                            nav.menu("#" + vQuery.menu);
-                                            if(vQuery.handler) vQuery.handler(vQuery, g);
-                                        };
-                                    }
+                                    });
+                                    res();
                                 });
-                                res();
-                            });
-                        }
-                    });
+                            }
+                        });
                 }));
             }
         };
-        
+
         nav.init();
 
         return nav;
     }
-  
- 
-    function asideToggle(){
-        return m("div", {class : "menuToggle"}, [m("button",{class : "mr-2"},m("span",{class: "material-symbols-outlined material-icons-48"},"menu"))]);
+
+
+    function asideToggle() {
+        return m("div", { class: "menuToggle" }, [m("button", { class: "mr-2" }, m("span", { class: "material-symbols-outlined material-icons-48" }, "menu"))]);
     }
-    function asideGlass(){
-        return m("div", {class : "screen-glass screen-glass-slide screen-glass-close"},[m("div",{class : "screen-glass-gray"})]);
+    function asideGlass() {
+        return m("div", { class: "screen-glass screen-glass-slide screen-glass-close" }, [m("div", { class: "screen-glass-gray" })]);
     }
     let navg;
 
     navigation.view = {
-        onupdate : function(x){
-           //  console.log(x.attrs.communityId);
+        onupdate: function (x) {
+
         },
-        oninit : function(x){
-           //  console.log(x.attrs.communityId);
-           if(!page.authenticated()) return;
-            // navg = navigable();
-            // page.navigable = navg;
-            
+        oninit: function (x) {
+            if (!page.authenticated()) return;
         },
-        oncreate : function (x) {
-            if(!page.authenticated()) return;
+        oncreate: function (x) {
+            if (!page.authenticated()) return;
             navg.addDrawerClickEvent("div.screen-glass-slide");
             navg.addDrawerClickEvent("div.menuToggle button");
         },
-        onremove : function(x){
-            //if(app) app.destroy();
+        onremove: function (x) {
+
         },
 
         view: function (vnode) {
-            //console.log("Render navigation");
-            if(!page.authenticated()) return;
+            if (!page.authenticated()) return;
             let crumb = "";
-            if(!vnode.attrs.hideBreadcrumb){
-                crumb =  m(page.components.breadCrumb);
+            if (!vnode.attrs.hideBreadcrumb) {
+                crumb = m(page.components.breadCrumb);
             }
             return [
-                m("nav", {class : "pageNav"}, [
+                m("nav", { class: "pageNav" }, [
                     asideToggle(),
-                    m(page.components.topMenu, {customTray: vnode.attrs.customTray}),
+                    m(page.components.topMenu, { customTray: vnode.attrs.customTray }),
                     asideGlass(),
-                    ,m(page.components.asideMenu)
+                    , m(page.components.asideMenu)
                 ]),
-               crumb
+                crumb
             ];
-            
+
 
         }
     };
--- src/AccountManagerUx7/client/components/object.js
+++ src/AccountManagerUx7/client/components/object.js
@@ -1,32 +1,32 @@
-(function(){
+(function () {
     let object = {};
- 
-    function renderObjectView(attrs){
+
+    function renderObjectView(attrs) {
         let object = attrs
-            return m("div",{class : "content-outer"},[ (fullMode ? "" : m(page.components.navigation)),
-              m("div", {class: "content-main"}, [
-                m("div", {class: "carousel"}, [
-                  m("div", {class : "carousel-inner"}, [
+        return m("div", { class: "content-outer" }, [(fullMode ? "" : m(page.components.navigation)),
+        m("div", { class: "content-main" }, [
+            m("div", { class: "carousel" }, [
+                m("div", { class: "carousel-inner" }, [
                     renderInnerObjectView(attrs),
-                    m("span", {onclick : toggleCarouselFull, class : "carousel-full"}, [m("span",{class : "material-icons"}, (fullMode ? "close_fullscreen" : "open_in_new"))]),
-                    m("span", {onclick : toggleCarouselMax, class : "carousel-max"}, [m("span",{class : "material-icons"}, (maxMode ? "photo_size_select_small" : "aspect_ratio"))]),
-                    m("span", {onclick : toggleInfo, class : "carousel-info"}, [m("span",{class : "material-icons" + (info ? "" : "-outlined")}, (info ? "info" : "info"))]),
-                    m("span", {onclick : function(){editItem(pr[pages.currentItem]);}, class : "carousel-edit"}, [m("span",{class : "material-icons"}, "edit")]),
-                    m("span", {onclick : toggleCarousel, class : "carousel-exit"}, [m("span",{class : "material-icons"},"close")])
-                  ])
+                    m("span", { onclick: toggleCarouselFull, class: "carousel-full" }, [m("span", { class: "material-icons" }, (fullMode ? "close_fullscreen" : "open_in_new"))]),
+                    m("span", { onclick: toggleCarouselMax, class: "carousel-max" }, [m("span", { class: "material-icons" }, (maxMode ? "photo_size_select_small" : "aspect_ratio"))]),
+                    m("span", { onclick: toggleInfo, class: "carousel-info" }, [m("span", { class: "material-icons" + (info ? "" : "-outlined") }, (info ? "info" : "info"))]),
+                    m("span", { onclick: function () { editItem(pr[pages.currentItem]); }, class: "carousel-edit" }, [m("span", { class: "material-icons" }, "edit")]),
+                    m("span", { onclick: toggleCarousel, class: "carousel-exit" }, [m("span", { class: "material-icons" }, "close")])
                 ])
-              ])
-            ]);
+            ])
+        ])
+        ]);
     }
-    function renderInnerObjectView(attrs){
-        
+    function renderInnerObjectView(attrs) {
+
         let cls = 'carousel-item';
         let object = attrs.object;
         let active = attrs.active;
-        if(active) cls += ' carousel-item-abs';
+        if (active) cls += ' carousel-item-abs';
         let maxMode = attrs.maxMode;
         let model = attrs.model || object?.model;
-        if(!object || !model){
+        if (!object || !model) {
             console.warn("Invalid object", object);
             return "";
         }
@@ -34,52 +34,50 @@
         let type = am7model.getModel(model);
         let objView = "";
         let mt = object.contentType || "";
-        if(object.profile && object.profile.portrait && object.profile.portrait.contentType){
+        if (object.profile && object.profile.portrait && object.profile.portrait.contentType) {
             let pp = object.profile.portrait;
             let path = g_application_path + "/media/" + am7client.dotPath(am7client.currentOrganization) + "/data.data" + pp.groupPath + "/" + pp.name;
-            if(active) objView = m("img",{class : "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), src : path});
+            if (active) objView = m("img", { class: "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), src: path });
         }
-        else if(model === 'note'){
-            objView = m("div", {class: "carousel-article-outer"}, m("div", {class: "carousel-article carousel-article-margin"}, object.text));
+        else if (model === 'note') {
+            objView = m("div", { class: "carousel-article-outer" }, m("div", { class: "carousel-article carousel-article-margin" }, object.text));
         }
-        else if(model === 'form'){
-            if(active) objView = m(page.components.form, attrs);
-           // objView = m("div", {class: "carousel-article-outer"}, m("div", {class: "carousel-article carousel-article-margin"}, object.text));
+        else if (model === 'form') {
+            if (active) objView = m(page.components.form, attrs);
         }
-        else if(model === 'message'){
-            if(active){
-                if(object.spoolStatus === 'SPOOLED' || object.spoolStatus === 'TRANSMITTED'){
+        else if (model === 'message') {
+            if (active) {
+                if (object.spoolStatus === 'SPOOLED' || object.spoolStatus === 'TRANSMITTED') {
                     object.spoolStatus = "ACKNOWLEGED_RECEIPT";
                     page.patchObject(object);
                 }
-                objView = m("div", {class: "carousel-article-outer"}, m("div", {class: "carousel-article carousel-article-margin"}, [
+                objView = m("div", { class: "carousel-article-outer" }, m("div", { class: "carousel-article carousel-article-margin" }, [
                     m("div", Base64.decode(object.data))
                 ]));
             }
         }
-        else if(mt.match(/^(video|image|audio)/)){
+        else if (mt.match(/^(video|image|audio)/)) {
 
             let path = g_application_path + "/media/" + am7client.dotPath(am7client.currentOrganization) + "/data.data" + object.groupPath + "/" + object.name;
-            if(mt.match(/^image/)){
-                ///if(i >= (pages.currentItem-1) && i <= (pages.currentItem + 1)) objView = m("img",{class : "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), src : path});
-                if(active) objView = m("img",{class : "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), src : path});
+            if (mt.match(/^image/)) {
+                if (active) objView = m("img", { class: "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), src: path });
             }
-            else{
-                if(mt.match(/mpeg3$/)) mt = "audio/mpeg";
-                if(active) objView = m((mt.match(/^video/) ? "video" : "audio"), {class: "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), preload: "auto", controls: "controls", autoplay: "autoplay"}, 
-                    m("source",{src: path, type: mt})
+            else {
+                if (mt.match(/mpeg3$/)) mt = "audio/mpeg";
+                if (active) objView = m((mt.match(/^video/) ? "video" : "audio"), { class: "carousel-item-img" + (maxMode ? " carousel-item-img-max" : ""), preload: "auto", controls: "controls", autoplay: "autoplay" },
+                    m("source", { src: path, type: mt })
                 );
             }
         }
-        else if(mt.match(/^text/)){
+        else if (mt.match(/^text/)) {
             let ctx = page.context();
-            if(active){
+            if (active) {
 
-                if(ctx.contextObjects[object.objectId] && ctx.contextObjects[object.objectId].detailsOnly){
+                if (ctx.contextObjects[object.objectId] && ctx.contextObjects[object.objectId].detailsOnly) {
                     logger.warn("Data is cached as detailsOnly.  This shouldn't have happened.");
                     delete ctx.contextObjects[object.objectId];
                 }
-                if(!ctx.contextObjects[object.objectId]){
+                if (!ctx.contextObjects[object.objectId]) {
                     console.log("Request full obj");
                     let q = am7view.viewQuery(am7model.newInstance(model));
                     q.field("objectId", object.objectId);
@@ -87,37 +85,30 @@
                         ctx.contextObjects[object.objectId] = (qr && qr.results.length ? qr.results[0] : null);
                         ctx.tags[object.objectId] = [];
                     });
-                    /*
-                    Promise.all([page.tags(model, object.objectId), page.search(q)]).then((a)=>{
-                        //ctx.contextObjects[object.objectId] = (q && q.results.length ? q.results[0] : null);
-                        m.redraw();
-                    });
-                    */
                 }
-                else{
+                else {
                     console.log("Use cached obj");
                     cls = 'carousel-item carousel-item-flex';
                     let dat = ctx.contextObjects[object.objectId];
-                    objView = m("div", {class: "carousel-article-outer"}, m("div", {class: "carousel-article carousel-article-margin"}, bbConverter.mInto(Base64.decode(dat.dataBytesStore))));
+                    objView = m("div", { class: "carousel-article-outer" }, m("div", { class: "carousel-article carousel-article-margin" }, bbConverter.mInto(Base64.decode(dat.dataBytesStore))));
                 }
             }
         }
 
-        else{
-            objView = m("div","Working on it: " + object.urn);
+        else {
+            objView = m("div", "Working on it: " + object.urn);
             console.warn(object);
         }
         let useCls = cls + " carousel-item-" + (active ? "100" : "0");
-        return m("div", {class : useCls}, objView);
+        return m("div", { class: useCls }, objView);
     }
 
     object.component = {
-        oncreate : function(vnode){
-            // console.log("Object Component Create");
+        oncreate: function (vnode) {
+
         },
-        view : function(vnode){
-            // console.log("Object Component View");
-            if(vnode.attrs.inner) return renderInnerObjectView(vnode.attrs);
+        view: function (vnode) {
+            if (vnode.attrs.inner) return renderInnerObjectView(vnode.attrs);
             else return renderObjectView(vnode.attrs);
         }
     };
