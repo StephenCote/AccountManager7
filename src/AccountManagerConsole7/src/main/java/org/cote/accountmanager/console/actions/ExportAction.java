33a64f8d0b6b6b68d9da555b610a92b9b59edc39
Stephen Cote <sw.cote@gmail.com>
2025-01-27 15:51:35 -0600

    Correctly extract bytes on export operation

--- src/AccountManagerConsole7/src/main/java/org/cote/accountmanager/console/actions/ExportAction.java
+++ src/AccountManagerConsole7/src/main/java/org/cote/accountmanager/console/actions/ExportAction.java
@@ -1,18 +1,24 @@
 package org.cote.accountmanager.console.actions;
 
+import java.util.ArrayList;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
+import org.cote.accountmanager.exceptions.FieldException;
 import org.cote.accountmanager.exceptions.ReaderException;
+import org.cote.accountmanager.exceptions.ValueException;
 import org.cote.accountmanager.io.IOSystem;
 import org.cote.accountmanager.io.Query;
 import org.cote.accountmanager.io.QueryResult;
 import org.cote.accountmanager.io.QueryUtil;
+import org.cote.accountmanager.io.stream.StreamSegmentUtil;
 import org.cote.accountmanager.record.BaseRecord;
 import org.cote.accountmanager.record.RecordFactory;
 import org.cote.accountmanager.schema.FieldNames;
 import org.cote.accountmanager.schema.ModelNames;
 import org.cote.accountmanager.schema.ModelSchema;
 import org.cote.accountmanager.schema.type.GroupEnumType;
+import org.cote.accountmanager.util.ByteModelUtil;
 import org.cote.accountmanager.util.ContentTypeUtil;
 import org.cote.accountmanager.util.FileUtil;
 
@@ -45,7 +51,7 @@ public class ExportAction extends CommonAction {
 					try {
 						exp = exportGroup(cmd, user, rec, cmd.getOptionValue(FieldNames.FIELD_TYPE), cmd.hasOption("recurse"));
 
-					} catch (ReaderException e) {
+					} catch (ReaderException | ValueException | FieldException e) {
 						logger.error(e);
 					}
 					
@@ -55,7 +61,7 @@ public class ExportAction extends CommonAction {
 		}
 	}
 	
-	private int exportGroup(CommandLine cmd, BaseRecord user, BaseRecord dir, String model, boolean recurse) throws ReaderException {
+	private int exportGroup(CommandLine cmd, BaseRecord user, BaseRecord dir, String model, boolean recurse) throws ReaderException, ValueException, FieldException {
 		int total = 0;
 		if(recurse) {
 			Query q = QueryUtil.createQuery(ModelNames.MODEL_GROUP, FieldNames.FIELD_PARENT_ID, dir.get(FieldNames.FIELD_ID));
@@ -73,16 +79,27 @@ public class ExportAction extends CommonAction {
 		return total;
 	}
 	
-	private void exportObject(CommandLine cmd, BaseRecord user, String model, BaseRecord object, String path) {
-		IOSystem.getActiveContext().getReader().populate(object);
+	private void exportObject(CommandLine cmd, BaseRecord user, String model, BaseRecord xobject, String path) throws ValueException, FieldException {
+		Query eq = QueryUtil.createQuery(model, FieldNames.FIELD_OBJECT_ID, xobject.get(FieldNames.FIELD_OBJECT_ID));
+		eq.planMost(true, new ArrayList<>());
+		BaseRecord object = IOSystem.getActiveContext().getSearch().findRecord(eq);
+
 		String upath = exportPath + path;
 		String fspath = upath + "/" + object.get(FieldNames.FIELD_NAME);
 		FileUtil.makePath(upath);
 		if(cmd.hasOption("extract")) {
 			if(model.equals(ModelNames.MODEL_DATA)) {
+				byte[] value = new byte[0];
+				if (object.hasField(FieldNames.FIELD_STREAM) && object.get(FieldNames.FIELD_STREAM) != null) {
+					BaseRecord stream = object.get(FieldNames.FIELD_STREAM);
+					StreamSegmentUtil ssu = new StreamSegmentUtil();
+					value = ssu.streamToEnd(stream.get(FieldNames.FIELD_OBJECT_ID), 0, 0);
+				} else {
+					value = ByteModelUtil.getValue(object);
+				}
+				
 				String fpath = fspath + "." + ContentTypeUtil.getExtensionFromType(object.get(FieldNames.FIELD_CONTENT_TYPE));
-				byte[] data = object.get(FieldNames.FIELD_BYTE_STORE);
-				FileUtil.emitFile(fpath, data);
+				FileUtil.emitFile(fpath, value);
 			}
 		}
 	}
