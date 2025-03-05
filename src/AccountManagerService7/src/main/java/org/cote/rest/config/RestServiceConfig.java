6d6f380d5a63e05cd2a66bb01b7da82b052c86ad
Stephen Cote <sw.cote@gmail.com>
2025-03-05 16:35:46 -0600

    Patch

--- src/AccountManagerService7/src/main/java/org/cote/rest/config/RestServiceConfig.java
+++ src/AccountManagerService7/src/main/java/org/cote/rest/config/RestServiceConfig.java
@@ -27,6 +27,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -34,6 +36,10 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
 
@@ -243,6 +249,23 @@ public class RestServiceConfig extends ResourceConfig{
 							logger.error(e);
 						}
 				}
+				
+				boolean disableSSLVerification = Boolean.parseBoolean(context.getInitParameter("ssl.verification.disabled"));
+				if(disableSSLVerification) {
+					TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+					    public X509Certificate[] getAcceptedIssuers(){return null;}
+					    public void checkClientTrusted(X509Certificate[] certs, String authType){}
+					    public void checkServerTrusted(X509Certificate[] certs, String authType){}
+					}};
+					try {
+					    SSLContext sc = SSLContext.getInstance("TLS");
+					    sc.init(null, trustAllCerts, new SecureRandom());
+					    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+					} catch (Exception e) {
+					    
+					}
+				}
+				
 				AM7LoginModule.setRoleMap(roleMap);
 			}
 			catch(SystemException e) {
