4f29ca980feeac2f6915b175cabf4655fdf09cee
Stephen Cote <sw.cote@gmail.com>
2025-06-20 12:05:11 -0500

    Change parameter name for clarity

--- src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/security/TokenService.java
+++ src/AccountManagerObjects7/src/main/java/org/cote/accountmanager/security/TokenService.java
@@ -362,18 +362,18 @@ public class TokenService {
 	/// This is still (hopefully obvious) very loose per the spec and likely very wrong
 	/// however, it's being used primarily for node-to-node communication versus trying to provide third party access
 	///
-	public static String createJWTToken(BaseRecord contextUser) throws ReaderException, IndexException{
-		return createJWTToken(contextUser, contextUser);
+	public static String createJWTToken(BaseRecord issuer) throws ReaderException, IndexException{
+		return createJWTToken(issuer, issuer);
 	}
-	public static String createJWTToken(BaseRecord contextUser, BaseRecord persona) throws ReaderException, IndexException{
-		return createJWTToken(contextUser, persona, UUID.randomUUID().toString(), TOKEN_EXPIRY_10_MINUTES);
+	public static String createJWTToken(BaseRecord issuer, BaseRecord persona) throws ReaderException, IndexException{
+		return createJWTToken(issuer, persona, UUID.randomUUID().toString(), TOKEN_EXPIRY_10_MINUTES);
 	}
-	public static String createJWTToken(BaseRecord contextUser, BaseRecord persona, String tokenId, int expiryMinutes) throws ReaderException, IndexException{
+	public static String createJWTToken(BaseRecord issuer, BaseRecord persona, String tokenId, int expiryMinutes) throws ReaderException, IndexException{
 		if(!personaType.matcher(persona.getSchema()).find()){
 			logger.error("Unsupported persona type: {0}", persona.getSchema());
 			return null;
 		}
-		CryptoBean bean = getCreateCipher(contextUser);
+		CryptoBean bean = getCreateCipher(issuer);
 		if(bean == null){
 			logger.error("Null security bean");
 			return null;
@@ -427,14 +427,14 @@ public class TokenService {
 		JwtBuilder builder = Jwts.builder();
 		builder.header().keyId(persona.get(FieldNames.FIELD_URN))
 			.add(CLAIM_SUBJECT_TYPE,persona.getSchema())
-			.add(CLAIM_ISSUER_URN, contextUser.get(FieldNames.FIELD_URN))
+			.add(CLAIM_ISSUER_URN, issuer.get(FieldNames.FIELD_URN))
 			.add(CLAIM_SBI, true)
 			.and()
 		;
 		
 		return builder
 		  .claims(claims.build())
-		  .issuer(contextUser.get(FieldNames.FIELD_URN))
+		  .issuer(issuer.get(FieldNames.FIELD_URN))
 		  .issuedAt(now)
 		  .expiration(expires)
 		  .subject(persona.get(FieldNames.FIELD_NAME))
