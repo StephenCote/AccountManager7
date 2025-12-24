e2dfdfe764e7cd47b739f91f71db1afa8de22531
Stephen Cote <sw.cote@gmail.com>
2025-12-24 14:18:20 -0600

    Correct config ref

--- src/AccountManagerService7/src/main/java/org/cote/rest/services/OlioService.java
+++ src/AccountManagerService7/src/main/java/org/cote/rest/services/OlioService.java
@@ -122,7 +122,7 @@ public class OlioService {
 		// sdu.generateSDImages(octx, Arrays.asList(char1, char2), cmd.getOptionValue("setting"), cmd.getOptionValue("style"), cmd.getOptionValue("bodyStyle"), Integer.parseInt(cmd.getOptionValue("reimage")), cmd.hasOption("export"), cmd.hasOption("hires"), seed);
 
 		BaseRecord a1 = null;
-		SDUtil sdu = new SDUtil(SDAPIEnumType.valueOf(context.getInitParameter("sd.apiType")), context.getInitParameter("sd.server"));
+		SDUtil sdu = new SDUtil(SDAPIEnumType.valueOf(context.getInitParameter("sd.server.apiType")), context.getInitParameter("sd.server"));
 		sdu.setDeferRemote(Boolean.parseBoolean(context.getInitParameter("task.defer.remote")));
 		Query q = QueryUtil.createQuery(OlioModelNames.MODEL_CHAR_PERSON, FieldNames.FIELD_OBJECT_ID, objectId);
 		q.planMost(true);
@@ -150,7 +150,7 @@ public class OlioService {
 		// sdu.generateSDImages(octx, Arrays.asList(char1, char2), cmd.getOptionValue("setting"), cmd.getOptionValue("style"), cmd.getOptionValue("bodyStyle"), Integer.parseInt(cmd.getOptionValue("reimage")), cmd.hasOption("export"), cmd.hasOption("hires"), seed);
 
 		BaseRecord a1 = null;
-		SDUtil sdu = new SDUtil(SDAPIEnumType.valueOf(context.getInitParameter("sd.apiType")), context.getInitParameter("sd.server"));
+		SDUtil sdu = new SDUtil(SDAPIEnumType.valueOf(context.getInitParameter("sd.server.apiType")), context.getInitParameter("sd.server"));
 
 		Query q = QueryUtil.createQuery(OlioModelNames.MODEL_CHAR_PERSON, FieldNames.FIELD_OBJECT_ID, objectId);
 		q.planMost(true);
